81a985aaa0c56d4da5efa87f20559a38fec8018d
VegeBun-csj <csj1071285741@gmail.com>
2022-06-23 22:56:47 +0800

    Fix some UI

--- src/pages/header.js
+++ src/pages/header.js
@@ -3,14 +3,16 @@ import "../styles/page-default.scss";
 import DoraTabs from "../components/tab/doratab";
 import Downdrop from "../components/downdrop";
 const Header = () => {
-    
+
     return (
         <div className="header-default" >
             <div className="nav-brand">
                 CORE Pallet
             </div>
             <DoraTabs></DoraTabs>
-            <Downdrop></Downdrop>
+            <div className="wrap-account">
+                <Downdrop></Downdrop>
+            </div>
         </div >
     );
 }
